e18e46fd6062e08e5dd71d4e132eb2ec9057e1e5
knutole <knutole@mapic.io>
2018-10-02 17:35:13 +0000

    remove open port

--- start-server.sh
+++ start-server.sh
@@ -13,7 +13,8 @@ if [ "$SHINY_FLOATING_LICENSE_SERVER_KEY" == "" ]; then
 fi
 
 # ensure docker container is running
-docker run --name $DOCKER_CONTAINER_NAME -p 8979 -t -d mapic/shiny-floating-license-server:latest bash 2> /dev/null
+# docker run --name $DOCKER_CONTAINER_NAME -p 8979 -t -d mapic/shiny-floating-license-server:latest bash 2> /dev/null
+docker run --name $DOCKER_CONTAINER_NAME -t -d mapic/shiny-floating-license-server:latest bash 2> /dev/null
 EXITCODE=$?
 if [ $EXITCODE -gt 0 ]; then
     docker start $DOCKER_CONTAINER_NAME
