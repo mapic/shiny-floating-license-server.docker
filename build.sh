619697e74462eb453fc921d8ef69738475417e64
knutole <knutole@mapic.io>
2018-10-11 22:40:53 +0000

    verify build

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# build
 docker build -t mapic/shiny-floating-license-server .
 
+# verify
+if [ "$?" -gt "0" ]; then
+    echo "Build errored."
+    exit 1
+fi
+
+# push
 docker push mapic/shiny-floating-license-server:latest
